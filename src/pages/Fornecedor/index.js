de37bff3bf49a2a2b3e54bb799dfbd205b62a891
elitonlunardi <lunardi.eliton@gmail.com>
2019-12-01 14:33:28 -0300

    correçoes no form de fornecedor

--- src/pages/Fornecedor/index.js
+++ src/pages/Fornecedor/index.js
@@ -31,8 +31,9 @@ export default class Fornecedor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      idEmpresa: "",
       fornecedores: [],
+      ehPessoaJuridica: false,
+      idEmpresa: "",
       modalShow: false,
       modalSetShow: false,
       acessandoApi: true
@@ -61,208 +62,295 @@ export default class Fornecedor extends Component {
     });
   };
 
+  validarPessoaFisica(values) {
+    console.log("entrou validação");
+    console.log(values.documento);
+    console.log(values.documento.length);
+    if (values.documento.length == 10) {
+      this.setState({
+        ehPessoaJuridica: false
+      });
+    } else if (values.documento.length == 13) {
+      console.log("juridica");
+      //87236945000195
+      this.setState({
+        ehPessoaJuridica: true
+      });
+    }
+  }
+
   render() {
-    const { modalShow, fornecedores, acessandoApi, idEmpresa } = this.state;
+    const {
+      modalShow,
+      fornecedores,
+      acessandoApi,
+      idEmpresa,
+      ehPessoaJuridica
+    } = this.state;
+
     const schema = yup.object({
+      ehPessoaJuridica: yup.boolean(),
       nome: yup.string().required("Nome é obrigatório"),
       documento: yup.string().required("Documento é obrigatório"),
-      rg: yup.string().required("Rg é obrigatório"),
-      dataNascimento: yup.string().required("Data de nascimento é obrigatório"),
-      telefone: yup.string().required("Telefone é obrigatório")
+      telefone: yup.string().required("Telefone é obrigatório"),
+      rg: yup.string().when("ehPessoaJuridica", {
+        is: value => value === true,
+        then: yup.string().max(5, "Rg é obrigatório"),
+        otherwise: yup.string().min(0, "a")
+      }),
+      dataNascimento: yup.string().required("Data de nascimento é obrigatória")
     });
 
     if (acessandoApi) {
       return <h1>Carregando...</h1>;
-    }
-
-    return (
-      <>
-        <NavbarComponent></NavbarComponent>
-        <Container>
-          <h2 className="textFornecedor">Fornecedores da empresa</h2>
-          <Row>
-            <Col>
-              <Card>
-                <Card.Body>
-                  <Button
-                    className="btnVincularFornecedor"
-                    variant="outline-success"
-                    onClick={this.handleShow}
-                  >
-                    Vincular
-                  </Button>
-                  <Table striped hover>
-                    <thead>
-                      <tr>
-                        <th>Documento</th>
-                        <th>Nome</th>
-                        <th>Data de Cadastro</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {fornecedores.map(forn => (
-                        <tr key={forn.id}>
-                          <td>{forn.documento}</td>
-                          <td>{forn.nome}</td>
-                          <td>{forn.dataCadastro}</td>
+    } else {
+      return (
+        <>
+          <NavbarComponent></NavbarComponent>
+          <Container>
+            <h2 className="textFornecedor">Fornecedores da empresa</h2>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Button
+                      className="btnVincularFornecedor"
+                      variant="outline-success"
+                      onClick={this.handleShow}
+                    >
+                      Vincular
+                    </Button>
+                    <Table striped hover>
+                      <thead>
+                        <tr>
+                          <th>Documento</th>
+                          <th>Nome</th>
+                          <th>Data de Cadastro</th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </Table>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+                      </thead>
+                      <tbody>
+                        {fornecedores.map(forn => (
+                          <tr key={forn.id}>
+                            <td>{forn.documento}</td>
+                            <td>{forn.nome}</td>
+                            <td>{forn.dataCadastro}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
 
-          <Modal show={modalShow} onHide={this.handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Vincular fornecedor</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Formik
-                validationSchema={schema}
-                onSubmit={data => {
-                  data.idEmpresa = idEmpresa;
-                  api
-                    .post(`Empresa/vincular-fornecedor`, data)
-                    .then(response => {
-                      if (response.status === 200) {
-                        swal(
-                          "Sucesso!",
-                          "Fornecedor vinculado com sucesso!",
-                          "success"
-                        );
-                        api
-                          .get(`Empresa/${data.idEmpresa}/obter-fornecedores`)
-                          .then(response => {
-                            this.setState({
-                              fornecedores: response.data.data
+            <Modal show={modalShow} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Vincular fornecedor</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Formik
+                  validationSchema={schema}
+                  onSubmit={data => {
+                    data.idEmpresa = idEmpresa;
+                    api
+                      .post(`Empresa/vincular-fornecedor`, data)
+                      .then(response => {
+                        if (response.status === 200) {
+                          swal(
+                            "Sucesso!",
+                            "Fornecedor vinculado com sucesso!",
+                            "success"
+                          );
+                          api
+                            .get(`Empresa/${data.idEmpresa}/obter-fornecedores`)
+                            .then(response => {
+                              this.setState({
+                                fornecedores: response.data.data
+                              });
                             });
-                          });
 
-                        this.handleClose();
-                      } else {
+                          this.handleClose();
+                        } else {
+                          swal(
+                            "Ooops :(",
+                            "Falha ao vincular o fornecedor.",
+                            "error"
+                          );
+                        }
+                      })
+                      .catch(error => {
+                        console.log(error.response);
                         swal(
                           "Ooops :(",
-                          "Falha ao vincular o fornecedor.",
+                          `${error.response.data.errors}`,
                           "error"
                         );
-                      }
-                    })
-                    .catch(error => {
-                      console.log(error.response);
-                      swal(
-                        "Ooops :(",
-                        `${error.response.data.errors}`,
-                        "error"
-                      );
-                    });
-                }}
-                initialValues={{
-                  nome: "",
-                  documento: "",
-                  rg: "",
-                  dataNascimento: "",
-                  telefone: ""
-                }}
-              >
-                {({
-                  handleSubmit,
-                  handleChange,
-                  handleBlur,
-                  values,
-                  touched,
-                  isValid,
-                  errors,
-                  setValues
-                }) => (
-                  <Form noValidate onSubmit={handleSubmit}>
-                    <Form.Group controlId="formGroupNome">
-                      <Form.Label>Nome</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Nome"
-                        name="nome"
-                        onChange={handleChange}
-                        value={values.nome}
-                        isInvalid={!!errors.nome}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.nome}
-                      </Form.Control.Feedback>
-                    </Form.Group>
+                      });
+                  }}
+                  initialValues={{
+                    ehPessoaJuridica: this.state.ehPessoaJuridica,
+                    nome: "",
+                    documento: "",
+                    rg: "",
+                    dataNascimento: "",
+                    telefone: ""
+                  }}
+                >
+                  {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    touched,
+                    isValid,
+                    errors,
+                    setValues
+                  }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                      {this.state.ehPessoaJuridica === true ? (
+                        <>
+                          <Form.Group controlId="formGroupNome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Nome"
+                              name="nome"
+                              onChange={handleChange}
+                              value={values.nome}
+                              isInvalid={!!errors.nome}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.nome}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="formGroupdataDocumento">
+                            <Form.Label>CNPJ/CPF</Form.Label>
+                            <Form.Control
+                              type="tel"
+                              placeholder="CNPJ/CPF"
+                              name="documento"
+                              onChange={handleChange}
+                              onKeyUp={this.validarPessoaFisica(values)}
+                              value={values.documento}
+                              isInvalid={!!errors.documento}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.documento}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="formGroupTelefone">
+                            <Form.Label>Telefone</Form.Label>
+                            <Form.Control
+                              type="tel"
+                              placeholder="Telefone"
+                              name="telefone"
+                              onChange={handleChange}
+                              value={values.telefone}
+                              isInvalid={!!errors.telefone}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.telefone}
+                            </Form.Control.Feedback>
+                          </Form.Group>
 
-                    <Form.Group controlId="formGroupdataDocumento">
-                      <Form.Label>CNPJ/CPF</Form.Label>
-                      <Form.Control
-                        type="tel"
-                        placeholder="CNPJ/CPF"
-                        name="documento"
-                        onChange={e => {
-                          handleChange(e);
-                        }}
-                        value={values.documento}
-                        isInvalid={!!errors.documento}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.documento}
-                      </Form.Control.Feedback>
-                    </Form.Group>
+                          <Form.Group controlId="formGroupRg">
+                            {!!values.ehPessoaJuridica}
+                            <Form.Label>RG</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="RG"
+                              name="rg"
+                              onChange={handleChange}
+                              value={values.rg}
+                              isInvalid={!!errors.rg}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.rg}
+                            </Form.Control.Feedback>
+                          </Form.Group>
 
-                    <Form.Group controlId="formGroupRg">
-                      <Form.Label>RG</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="RG"
-                        name="rg"
-                        onChange={handleChange}
-                        value={values.rg}
-                        isInvalid={!!errors.rg}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.rg}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-
-                    <Form.Group controlId="formGroupdataNascimento">
-                      <Form.Label>Data de nascimento</Form.Label>
-                      <Form.Control
-                        type="date"
-                        placeholder="Data de nascimento"
-                        name="dataNascimento"
-                        onChange={handleChange}
-                        value={values.dataNascimento}
-                        isInvalid={!!errors.dataNascimento}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.dataNascimento}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-
-                    <Form.Group controlId="formGroupdataNascimento">
-                      <Form.Label>Telefone</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Telefone"
-                        name="telefone"
-                        onChange={handleChange}
-                        value={values.telefone}
-                        isInvalid={!!errors.telefone}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.telefone}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-
-                    <Button type="submit">Vincular</Button>
-                  </Form>
-                )}
-              </Formik>
-            </Modal.Body>
-            <Modal.Footer></Modal.Footer>
-          </Modal>
-        </Container>
-      </>
-    );
+                          <Form.Group controlId="formGroupdataNascimento">
+                            {!!values.ehPessoaJuridica}
+                            <Form.Label>Data de nascimento</Form.Label>
+                            <Form.Control
+                              type="date"
+                              placeholder="Data de nascimento"
+                              name="dataNascimento"
+                              onChange={handleChange}
+                              value={values.dataNascimento}
+                              isInvalid={!!errors.dataNascimento}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.dataNascimento}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Button type="submit">Vincular</Button>
+                        </>
+                      ) : (
+                        <>
+                          <Form.Group controlId="formGroupNome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Nome"
+                              name="nome"
+                              onChange={handleChange}
+                              value={values.nome}
+                              isInvalid={!!errors.nome}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.nome}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="formGroupdataDocumento">
+                            <Form.Label>CNPJ/CPF</Form.Label>
+                            <Form.Control
+                              type="tel"
+                              placeholder="CNPJ/CPF"
+                              name="documento"
+                              onChange={e => {
+                                handleChange(e);
+                                this.validarPessoaFisica(values);
+                              }}
+                              value={values.documento}
+                              isInvalid={!!errors.documento}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.documento}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="formGroupTelefone">
+                            <Form.Label>Telefone</Form.Label>
+                            <Form.Control
+                              type="tel"
+                              placeholder="Telefone"
+                              name="telefone"
+                              onChange={handleChange}
+                              value={values.telefone}
+                              isInvalid={!!errors.telefone}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.telefone}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                          <Button type="submit">Vincular</Button>
+                        </>
+                      )}
+                      <Button
+                        type="button"
+                        onClick={e => this.teste(values, setValues)}
+                      >
+                        Mudar state
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </Modal.Body>
+              <Modal.Footer></Modal.Footer>
+            </Modal>
+          </Container>
+        </>
+      );
+    }
   }
 }
